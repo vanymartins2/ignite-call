9ca5b6e9c106ccaa162e7c12f35d5fe24cb48bef
Vanessa Martins <vanymartins2@gmail.com>
2023-04-17 10:42:23 -0300

    Deploy to vercel

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,68 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { prisma } from '@/src/lib/prisma'
+
+import { ScheduleForm } from './ScheduleForm'
+
+import { Container, UserHeader } from './styles'
+import { NextSeo } from 'next-seo'
+
+type ScheduleProps = {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <>
+      <NextSeo title={`Agendar com ${user.name} | Ignite Call`} />
+
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.name}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+
+        <ScheduleForm />
+      </Container>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+
+  if (!user) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url
+      }
+    },
+    revalidate: 60 * 60 * 24 // 1 dia
+  }
+}
